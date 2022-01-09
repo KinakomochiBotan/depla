ae3f677d2404de9966f120d501ad4068cf12d5d3
r19018 <r19018@osaka-pct.ac.jp>
2022-01-10 01:05:36 +0900

    Changed Cargo.toml Updated anyhow v1.0.51 to v 1.0.52 Updated tokio v1.14.0 to v1.15.0 Updated numpy v0.15.0 to v1.15.1

--- run/python/dataset.py
+++ run/python/dataset.py
@@ -6,7 +6,7 @@ import wthor
 class WTHORDataset(Dataset):
     def __init__(self, paths, device):
         data = wthor.parse(paths)
-        generator = ((torch.from_numpy(data).to(device), torch.from_numpy(label).to()) for (data, label) in data)
+        generator = ((torch.from_numpy(data).to(device), torch.from_numpy(label).to(device)) for (data, label) in data)
         self.__train = tuple(generator)
 
     def __len__(self):
@@ -14,3 +14,6 @@ class WTHORDataset(Dataset):
 
     def __getitem__(self, item):
         return self.__train[item]
+
+
+print(len(WTHORDataset(["../wthor/WTH_%d.wtb" % year for year in range(2010, 2021)], "cpu")))
